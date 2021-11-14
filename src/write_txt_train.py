251c84d17609d2b33d3dfe8b7c9897132ed56326
ericsdata <eachristiansen1@gmail.com>
2021-11-14 18:29:23 -0500

    data extract, formatting, to Colab

---
+++ src/write_txt_train.py
@@ -0,0 +1,51 @@
+
+from typing import Sequence
+import db_ec
+import pandas as pd
+import numpy as np
+import math
+
+
+conn = db_ec.connect_db(r'..\data\beerdb.sqlite')
+
+## REad in data
+dat = pd.read_sql('SELECT beerID, brewerId, overall, review_text, style FROM reviews;',conn)
+## Simplify targets
+dat['style'] = dat['style'].str.replace('India.*', 'India Pale Ale', regex = True)
+dat['style'] = dat['style'].str.replace('Belgi.*', 'Witbier', regex = True)
+## Convert to numeric ID for NN
+dat['style_id'] = dat['style'].map({
+                        'Dunkel' : 1, 
+                        'Pilsener':2, 
+                        'Low Alcohol': 3, 
+                        'India Pale Ale':4,
+                        'Oktoberfest/Märzen' : 5, 
+                        'American Pale Ale':6, 
+                        'Schwarzbier':7,
+                        'Witbier' : 8, 
+                        'Classic German Pilsener' : 9, 
+                        'Scottish Ale' : 10, 
+                        'Fruit Beer' :11,
+                        'Pale Lager' : 12, 
+                        'Premium Lager':13, 
+                        'Dunkler Bock':14, 
+                        'Heller Bock' :15})
+
+## Fix the rating format
+def adjustOverallScore(unformatted_score, round = ['Up','Down','NoRound']):
+    score = float(unformatted_score.split('/20')[0]) / 20 ### Split and format
+    if round == 'Down':
+        f_score = round(score,1)
+    elif round == 'Up':
+        f_score = round(score,1)
+    else:
+        f_score = float(score)
+
+    
+    return f_score
+
+
+dat['overall_f'] = dat['overall'].apply(adjustOverallScore, 'NoRound')
+
+### MAke a training set
+from sklearn.model_selection import train_test_split
